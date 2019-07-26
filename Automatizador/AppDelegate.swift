2e962860b7d585c180b879be75f7b24229b9e2c9
Maicon Dias Castro <mdcastro@stefanini.com>
2019-07-26 16:47:08 -0300

    Initial Commit

---
+++ Automatizador/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Automatizador
+//
+//  Created by User on 26/07/19.
+//  Copyright © 2019 Stefanini. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
